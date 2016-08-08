e7c480a11d9762713cf7031dab8366641bf17518
Christopher W York <christopher.w.york@gmail.com>
2016-08-08 11:20:55 +0100

    Canvas globe, some visual artefacts.

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 import * as d3 from 'd3'
 
-import { geoOrthographic, geoPath } from 'd3-geo'
+import { geoAzimuthalEqualArea, geoPath } from 'd3-geo'
 import { feature } from 'topojson'
 import { queue } from 'd3-queue'
 
 import './css/styles.css!'
 
-
 const width = 800
 const height = 600
+
+let projection = geoAzimuthalEqualArea()
+  .scale(250)
+  .translate([width / 2, height / 2])
+  .clipAngle(90)
+
+let canvas = d3.select('body').append('canvas')
+  .attr('width', width)
+  .attr('height', height)
+
+let context = canvas.node().getContext('2d')
+
+let path = geoPath()
+  .projection(projection)
+  .context(context)
 
 queue()
   .defer(d3.json, 'data/world-topo.json')
   .await(function(err, world) {
     if (err) return console.error(err)
 
-    let projection = geoOrthographic()
-      .scale(250)
-      .translate([width / 2, height / 2])
-      .clipAngle(90)
-
-    let path = geoPath()
-      .projection(projection)
-
-    let svg = d3.select('body').append('svg')
-      .attr('width', width)
-      .attr('height', height)
-
-    svg.append('path')
-      .attr('class', 'globe')
-      .datum(feature(world, world.objects.countries))
-      .attr('d', path)
+    path(feature(world, world.objects.countries))
+    context.stroke()
 })
